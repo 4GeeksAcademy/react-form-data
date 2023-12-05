c1adb65e101e3b0ed2373fb03825d3c2c42ff62a
Shane B <dotfortun@users.noreply.github.com>
2023-12-05 23:50:16 +0000

    Minimal file upload example.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  // We make a ref that will point to the file input element.
+  const fileInput = useRef(null);
+  const [resp, setResp] = useState({});
+
+  const sendFormData = async (e) => {
+    // We need to prevent the form from reloading the page.
+    // Because I was too lazy to use onClick on the button.
+    e.preventDefault();
+
+    // We make a new FormData() object to store the form data.
+    const form = new FormData();
+
+    // Then we append the files from the input element.
+    form.append("file", fileInput.current.files[0]);
+
+    // Then we can send the files along with the form data.
+    const r = await fetch("https://httpbin.org/anything", {
+      method: "POST",
+      Accept: "application/json",
+      "Content-Type": "multipart/form-data",
+      body: form,
+    });
+
+    setResp(await r.json());
+  };
+
+  return (
+    <div className="container">
+      {/* This onSubmit just handles the interaction. */}
+      <form onSubmit={sendFormData}>
+        {/* The ref we made earlier points to this input with the ref prop: */}
+        <input type="file" name="media" accept="image/*" ref={fileInput} />
+        <button className="btn btn-primary">Submit</button>
+      </form>
+      <code>{JSON.stringify(resp)}</code>
+    </div>
+  );
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,12 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import { StrictMode } from "react/cjs/react.production.min.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(
+  <StrictMode>
+    <Home />
+  </StrictMode>,
+  document.querySelector("#app")
+);
